d3132d317fac803f4e916dab522192a8f503968e
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-02 05:07:18 +0600

    minor adjustments

--- app/dashboard/(user)/settings/layout.tsx
+++ app/dashboard/(user)/settings/layout.tsx
@@ -1,27 +1,4 @@
 import Link from "next/link"
-import { CircleUser, Menu, Package2, Search } from "lucide-react"
-
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
-
 export default function UserSettingsLayout({
   children,
 }: {
@@ -33,7 +10,7 @@ export default function UserSettingsLayout({
         <h1 className="text-3xl font-cal">User Settings</h1>
       </div>
       <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav className="grid gap-4 text-sm text-muted-foreground">
+        <nav className="grid gap-4 text-sm text-geist-900">
           <Link href="#" className="font-semibold text-primary">
             General
           </Link>
