8522225b5d15646b7057928824b7a5cfba0a066a
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-02 05:07:58 +0600

    added dummy components

--- app/dashboard/[slug]/page.tsx
+++ app/dashboard/[slug]/page.tsx
@@ -1,26 +1,292 @@
-import { getShopDetails } from "@/lib/actions/shop"
-import { Button } from "@/components/ui/button"
+"use client"
 
-export default async function ShopHome({
+import Link from "next/link"
+import {
+  Activity,
+  ArrowUpRight,
+  CircleUser,
+  CreditCard,
+  DollarSign,
+  Menu,
+  Package2,
+  Search,
+  Users,
+} from "lucide-react"
+
+import { getShopDetails } from "@/lib/actions/shop"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet"
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+export default async function Dashboard({
   params,
 }: {
   params: { slug: string }
 }) {
-  const shop: any = await getShopDetails(params.slug)
-
+  // const shop: any = await getShopDetails(params.slug)
   return (
-    <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-      <div className="flex items-center">
-        <h1 className="font-cal text-lg md:text-2xl">{shop.name} Overview</h1>
+    <main className="flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8">
+      <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-md font-cal">Total Revenue</CardTitle>
+            <DollarSign className="h-4 w-4 text-geist-900" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-cal">$45,231.89</div>
+            <p className="text-xs text-geist-900">+20.1% from last month</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-md font-cal">Subscriptions</CardTitle>
+            <Users className="h-4 w-4 text-geist-900" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-cal">+2350</div>
+            <p className="text-xs text-geist-900">+180.1% from last month</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-md font-cal">Sales</CardTitle>
+            <CreditCard className="h-4 w-4 text-geist-900" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-cal">+12,234</div>
+            <p className="text-xs text-geist-900">+19% from last month</p>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-md font-cal">Active Now</CardTitle>
+            <Activity className="h-4 w-4 text-geist-900" />
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-cal">+573</div>
+            <p className="text-xs text-geist-900">+201 since last hour</p>
+          </CardContent>
+        </Card>
       </div>
-      <div className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm">
-        <div className="flex flex-col items-center gap-1 text-center">
-          <h3 className="text-2xl font-cal">You have no products</h3>
-          <p className="text-sm text-geist-900">
-            You can start selling as soon as you add a product.
-          </p>
-          <Button className="mt-4">Add Product</Button>
-        </div>
+      <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+        <Card className="xl:col-span-2">
+          <CardHeader className="flex flex-row items-center">
+            <div className="grid gap-2">
+              <CardTitle>Transactions</CardTitle>
+              <CardDescription>
+                Recent transactions from your store.
+              </CardDescription>
+            </div>
+            <Button asChild size="sm" className="ml-auto gap-1">
+              <Link href="#">
+                View All
+                <ArrowUpRight className="h-4 w-4" />
+              </Link>
+            </Button>
+          </CardHeader>
+          <CardContent>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead>Customer</TableHead>
+                  <TableHead className="hidden xl:table-column">Type</TableHead>
+                  <TableHead className="hidden xl:table-column">
+                    Status
+                  </TableHead>
+                  <TableHead className="hidden xl:table-column">Date</TableHead>
+                  <TableHead className="text-right">Amount</TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-cal">Liam Johnson</div>
+                    <div className="hidden text-sm text-geist-900 md:inline">
+                      liam@example.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">Sale</TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    <Badge className="text-xs" variant="outline">
+                      Approved
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                    2023-06-23
+                  </TableCell>
+                  <TableCell className="text-right">$250.00</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-cal">Olivia Smith</div>
+                    <div className="hidden text-sm text-geist-900 md:inline">
+                      olivia@example.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    Refund
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    <Badge className="text-xs" variant="outline">
+                      Declined
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                    2023-06-24
+                  </TableCell>
+                  <TableCell className="text-right">$150.00</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-cal">Noah Williams</div>
+                    <div className="hidden text-sm text-geist-900 md:inline">
+                      noah@example.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    Subscription
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    <Badge className="text-xs" variant="outline">
+                      Approved
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                    2023-06-25
+                  </TableCell>
+                  <TableCell className="text-right">$350.00</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-cal">Emma Brown</div>
+                    <div className="hidden text-sm text-geist-900 md:inline">
+                      emma@example.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">Sale</TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    <Badge className="text-xs" variant="outline">
+                      Approved
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                    2023-06-26
+                  </TableCell>
+                  <TableCell className="text-right">$450.00</TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>
+                    <div className="font-cal">Liam Johnson</div>
+                    <div className="hidden text-sm text-geist-900 md:inline">
+                      liam@example.com
+                    </div>
+                  </TableCell>
+                  <TableCell className="hidden xl:table-column">Sale</TableCell>
+                  <TableCell className="hidden xl:table-column">
+                    <Badge className="text-xs" variant="outline">
+                      Approved
+                    </Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell lg:hidden xl:table-column">
+                    2023-06-27
+                  </TableCell>
+                  <TableCell className="text-right">$550.00</TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </CardContent>
+        </Card>
+        <Card>
+          <CardHeader>
+            <CardTitle>Recent Sales</CardTitle>
+          </CardHeader>
+          <CardContent className="grid gap-8">
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/01.png" alt="Avatar" />
+                <AvatarFallback>OM</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-cal leading-none">Olivia Martin</p>
+                <p className="text-sm text-geist-900">
+                  olivia.martin@email.com
+                </p>
+              </div>
+              <div className="ml-auto font-cal">+$1,999.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/02.png" alt="Avatar" />
+                <AvatarFallback>JL</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-cal leading-none">Jackson Lee</p>
+                <p className="text-sm text-geist-900">jackson.lee@email.com</p>
+              </div>
+              <div className="ml-auto font-cal">+$39.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/03.png" alt="Avatar" />
+                <AvatarFallback>IN</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-cal leading-none">Isabella Nguyen</p>
+                <p className="text-sm text-geist-900">
+                  isabella.nguyen@email.com
+                </p>
+              </div>
+              <div className="ml-auto font-cal">+$299.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/04.png" alt="Avatar" />
+                <AvatarFallback>WK</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-cal leading-none">William Kim</p>
+                <p className="text-sm text-geist-900">will@email.com</p>
+              </div>
+              <div className="ml-auto font-cal">+$99.00</div>
+            </div>
+            <div className="flex items-center gap-4">
+              <Avatar className="hidden h-9 w-9 sm:flex">
+                <AvatarImage src="/avatars/05.png" alt="Avatar" />
+                <AvatarFallback>SD</AvatarFallback>
+              </Avatar>
+              <div className="grid gap-1">
+                <p className="text-sm font-cal leading-none">Sofia Davis</p>
+                <p className="text-sm text-geist-900">sofia.davis@email.com</p>
+              </div>
+              <div className="ml-auto font-cal">+$39.00</div>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </main>
   )
