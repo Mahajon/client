10a7fd3c8f2c563fbf93d3ea95dbb667c4a20edd
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-19 11:54:10 +0600

    search form color

--- components/dashboard/products/list/search.tsx
+++ components/dashboard/products/list/search.tsx
@@ -28,7 +28,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         Search
       </label>
       <input
-        className="peer block w-full h-8 rounded-md bg-primary border py-[9px] pl-10 text-sm outline-2 placeholder:text-muted-foreground"
+        className="peer block w-full h-8 rounded-md bg-secondary border py-[9px] pl-10 text-sm outline-2 placeholder:text-muted-foreground"
         placeholder={placeholder}
         onChange={(e) => {
           handleSearch(e.target.value)
