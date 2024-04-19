cc8b7cf06b5b1fa6d2d68a847121034754e7aa08
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-19 11:32:00 +0600

    removed nav and social links

--- config/site.ts
+++ config/site.ts
@@ -3,27 +3,4 @@ export type SiteConfig = typeof siteConfig
 export const siteConfig = {
   name: "Mahajon",
   description: "All-in-one platform for your f-commerce business.",
-  mainNav: [
-    {
-      title: "Home",
-      href: "/",
-    },
-    {
-      title: "Features",
-      href: "/features",
-    },
-    {
-      title: "Pricing",
-      href: "/pricing",
-    },
-    {
-      title: "Blog",
-      href: "/blog",
-    },
-  ],
-  links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/ui",
-    docs: "https://ui.shadcn.com",
-  },
 }
