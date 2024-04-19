691ac2781ae1be24bfd672fa3ec0e32a1cefb75a
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-19 12:58:44 +0600

    added coming soon and maintenance pages and middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,19 @@
 import { cookies } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
+import { get } from "@vercel/edge-config"
 
 export async function middleware(request: NextRequest) {
+  if (process.env.COMING_SOON === "true") {
+    request.nextUrl.pathname = "/coming-soon"
+    return NextResponse.rewrite(request.nextUrl)
+  }
+  if (process.env.VERCEL_ENV === "production") {
+    const isInMaintenanceMode = await get("isInMaintenanceMode")
+    if (isInMaintenanceMode) {
+      request.nextUrl.pathname = "/maintenance"
+      return NextResponse.rewrite(request.nextUrl)
+    }
+  }
   const requestHeaders = new Headers(request.headers)
   requestHeaders.set("x-url", request.url)
   if (!cookies().get("next-auth.session-token"))
@@ -16,5 +28,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: [
+    "/((?!api|_next/static|_next/image|.*\\.png$|.*\\.ico|logo/.*\\.svg$).*)",
+  ],
 }
