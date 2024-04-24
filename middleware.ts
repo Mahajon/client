9f3f11bd49e7f04eeb2d83a8b5734bb7b4be4b2f
Fardeen Ehsan <fardeen.es7@gmail.com>
2024-04-24 12:53:51 +0600

    fixed auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { cookies } from "next/headers"
 import { NextRequest, NextResponse } from "next/server"
 import { get } from "@vercel/edge-config"
+import { getToken } from "next-auth/jwt"
 
-export async function middleware(request: NextRequest) {
+export default async function middleware(request: NextRequest) {
   if (process.env.COMING_SOON === "true") {
     request.nextUrl.pathname = "/coming-soon"
     return NextResponse.rewrite(request.nextUrl)
@@ -14,12 +15,23 @@ export async function middleware(request: NextRequest) {
       return NextResponse.rewrite(request.nextUrl)
     }
   }
-  const requestHeaders = new Headers(request.headers)
-  requestHeaders.set("x-url", request.url)
-  if (!cookies().get("next-auth.session-token"))
-    if (request.nextUrl.pathname.startsWith("/dashboard")) {
+
+  if (request.nextUrl.pathname.startsWith("/dashboard")) {
+    const token = await getToken({
+      req: request,
+      secret: process.env.NEXTAUTH_SECRET,
+    })
+    if (!token) {
       return NextResponse.rewrite(new URL("/login", request.url))
     }
+  }
+
+  const requestHeaders = new Headers(request.headers)
+  requestHeaders.set("x-url", request.url)
+  // if (!cookies().get("next-auth.session-token"))
+  //   if (request.nextUrl.pathname.startsWith("/dashboard")) {
+  //     return NextResponse.rewrite(new URL("/login", request.url))
+  //   }
   return NextResponse.next({
     request: {
       headers: requestHeaders,
